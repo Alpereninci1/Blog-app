8298742c9a31fb5096b10d0cf60f7b05dbb83019
Alperen İnci <alperen25inci@hotmail.com>
2022-11-04 11:35:35 +0300

    User and Auth Controller Configured

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,84 +1,58 @@
 <?php
-
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Traits\ApiResponser;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
+    use ApiResponser;
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
+     * Login the user.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function login(Request $request)
     {
-        //
+        //Validate data
+        $data = $request->only('username', 'password');
+        $validator = Validator::make($data, [
+            'username' => 'required|string|max:255',
+            'password' => 'required|string',
+        ]);
+        if($validator->fails()){
+            return $this->errorResponse($validator->messages(), Response::HTTP_NON_AUTHORITATIVE_INFORMATION);
+        }
+        $user = User::where('email', $request->username)
+            ->orWhere('username', $request->username)->first();
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            return $this->errorResponse(['username' => ['The provided credentials are incorrect.']], Response::HTTP_NON_AUTHORITATIVE_INFORMATION);
+        }
+        $token = $user->createToken($request->username)->plainTextToken;
+        $user['authorisation'] = ([
+            'token' => $token,
+            'type' => 'bearer',
+        ]);
+        return $this->successResponse($user, 'User Successfully Login');
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
+     * Logout the specified user.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function logout(Request $request)
     {
-        //
+        $request->user()->currentAccessToken()->delete();
+        return $this->successResponse(null,'Successfully logged out');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,83 +2,144 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Traits\ApiResponser;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
 
 class UserController extends Controller
 {
+    use ApiResponser;
+    // Store login User id;
+    private $loginuserid;
+
     /**
-     * Display a listing of the resource.
+     * Assign Auth user id to login user id variable.
+     *
+     */
+
+
+    /**
+     * Get the specified resource in storage.
      *
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        //
+        $users = User::with('roles')->get();
+        return $this->successResponse($users);
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
+     * Store the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\User  $user
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        //Validate data
+        $data = $request->only('name', 'email', 'username', 'password');
+        $validator = Validator::make($data, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'username' => 'required|string|max:255|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+        if($validator->fails()){
+            return $this->errorResponse($validator->messages(), Response::HTTP_NON_AUTHORITATIVE_INFORMATION);
+        }
+        $data['created_user_id'] = $this->loginuserid;
+        $data['updated_user_id'] = $this->loginuserid;
+        $data['password'] = Hash::make($request->password);
+        $user = User::create($data);
+        if($user) {
+            return $this->successResponse($user, 'User Successfully Created', Response::HTTP_CREATED);
+        }
+        return $this->errorResponse('Date not store, kindly do this request again', Response::HTTP_UNPROCESSABLE_ENTITY);
     }
 
     /**
-     * Display the specified resource.
+     * Show the specified resource in storage.
      *
-     * @param  int  $id
+     * @param  \App\Models\User  $user
      * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
-        //
+        $user = User::find($id);
+        if($user)
+        {
+            $user->roles->pluck('name');
+            return $this->successResponse($user);
+        }
+        return $this->errorResponse('Page Not Found',Response::HTTP_NOT_FOUND);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \App\Models\User  $user
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
     {
-        //
+        $user = User::find($id);
+        if($user)
+        {
+            //Validate data
+            $data = $request->only('name', 'email', 'username', 'password');
+            $validator = Validator::make($data, [
+                'name' => 'nullable|string|max:255',
+                'email' => 'nullable|string|email|max:255|unique:users',
+                'username' => 'nullable|string|max:255|unique:users',
+                'password' => 'nullable|string|min:6',
+            ]);
+            if($validator->fails()){
+                return $this->errorResponse($validator->messages(), Response::HTTP_NON_AUTHORITATIVE_INFORMATION);
+            }
+            if(isset($data['password']))
+                $data['password'] = Hash::make($data['password']);
+            $data['updated_user_id'] = $this->loginuserid;
+            $status = $user->update($data);
+            if($status)
+            {
+                $user->roles->pluck('name');
+                return $this->successResponse($user, 'User Successfully Updated');
+            }
+            return $this->errorResponse('Date not store, kindly do this request again', Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+        return $this->errorResponse('Page Not Found',Response::HTTP_NOT_FOUND);
     }
 
+
     /**
-     * Remove the specified resource from storage.
+     * Remove the specified resource in storage.
      *
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\User  $user
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
-        //
+        $user = User::find($id);
+        if($user)
+        {
+            $data = (['updated_user_id' => $this->loginuserid]);
+            $user->update($data);
+            $user->syncRoles([]);
+            $user->delete();
+            return $this->successResponse(null,'User Deleted Successfully');
+        }
+        return $this->errorResponse('Page Not Found',Response::HTTP_NOT_FOUND);
     }
+
+
 }
